2aa24081c6d8c1a1ba985cd549769ebb393479b7
Bahram2006 <yardam034@gmail.com>
2026-06-12 14:53:41 +0500

    admin backend full clean code

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/clinik`)
+    mongoose.connection.on('connected', () => console.log("Database Connected"));
+    
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
 }
 
-export default connectDB
+export default connectDB;
