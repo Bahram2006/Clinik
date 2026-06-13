0de788a38ad080bb411d79901a0bcb9251fcbd4f
Bahram2006 <yardam034@gmail.com>
2026-06-13 10:32:41 +0500

    admin started code

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AdminContextProvider from "./context/AdminContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AdminContextProvider>
+      <App />
+    </AdminContextProvider>
   </BrowserRouter>,
-)
+);
