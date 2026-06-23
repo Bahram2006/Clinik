0c254a54e2de153e62b58f14a6b59050fee8cd6d
Bahram2006 <yardam034@gmail.com>
2026-06-23 16:25:17 +0500

    razorpay all clean code proects

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -55,6 +55,23 @@ const MyAppointments = () => {
 
   }
 
+  const appointmentRazorpay = async (appointmentId) => {
+
+    try {
+      
+      const {data} = await axios.post(backendUrl+'/api/user/payment-razorpay',{appointmentId},{headers:{token}})
+
+      if (data.success) {
+        console.log(data.order)
+        
+      }
+
+    } catch (error) {
+      
+    }
+
+  }
+
   useEffect(()=>{
     if (token) {
       getUserAppointments()
@@ -79,7 +96,7 @@ const MyAppointments = () => {
               <p className="text-xs mt-1"><span className="text-sm text-neutral-700 font-medium">Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
             </div>
             <div className="flex flex-col gap-2 justify-end">
-              {!item.cancelled && <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button> }
+              {!item.cancelled && <button onClick={()=>appointmentRazorpay(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button> }
               {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">Cancel appointment</button> }
               {item.cancelled && <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment cancelled</button>}
             </div>
