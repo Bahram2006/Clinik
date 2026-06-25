8f89cdb40d98d6709d9d57201cc77bdac091fb53
Bahram2006 <yardam034@gmail.com>
2026-06-25 15:25:01 +0500

    user appointments age name finished code

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -3,10 +3,12 @@
 import { useContext } from "react"
 import { AdminContext } from "../../context/AdminContext"
 import { useEffect } from "react"
+import { AppContext } from "../../context/AppContext"
 
 const AllAppointments = () => {
 
   const {aToken, appointments, getAllAppointments} = useContext(AdminContext)
+  const {calculateAge} = useContext(AppContext)
 
   useEffect(()=>{
     if (aToken) {
@@ -15,13 +17,13 @@ const AllAppointments = () => {
   },[aToken])
 
   return (
-    <div className="text-clip flex flex-wrap">
+    <div className="w-full max-w-6xl m-5">
 
-      <p>All Appointments</p>
+      <p className="mb-3 text-lg font-medium">All Appointments</p>
 
-      <div>
+      <div className="bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll">
 
-        <div>
+        <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
           <p>#</p>
           <p>Patient</p>
           <p>Age</p>
@@ -30,6 +32,16 @@ const AllAppointments = () => {
           <p>Fees</p>
           <p>Actions</p>
         </div>
+
+        {appointments.map((item,index)=>(
+          <div className="flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50" key={index}>
+            <p className="max-sm:hidden">{index+1}</p>
+            <div className="flex items-center gap-2">
+              <img className="w-8 rounded-full" src={item.userData.image} alt="" /> <p>{item.userData.name}</p>
+            </div>
+            <p>{calculateAge(item.userData)}</p>
+          </div>
+        ))}
 
       </div>
     </div>
