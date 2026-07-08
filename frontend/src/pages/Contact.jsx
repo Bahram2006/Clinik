db5954f09745be6d2e903e6335a3db8d74c66083
Bahram2006 <yardam034@gmail.com>
2026-07-08 11:41:14 +0500

    translation code updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,13 +1,18 @@
 // import React from 'react'
-
 import { assets } from "../assets/assets";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="text-center text-2xl pt-10 text-gray-500">
         <p>
-          CONTACT <span className="text-gray-700 font-semibold">US</span>
+          {t("contact.title_line1")}{" "}
+          <span className="text-gray-700 font-semibold">
+            {t("contact.title_line2")}
+          </span>
         </p>
       </div>
 
@@ -19,21 +24,22 @@ const Contact = () => {
         />
 
         <div className="flex flex-col justify-center items-start gap-6">
-          <p className="font-semibold text-lg text-gray-600">Our OFFICE</p>
-          <p className="text-gray-500">
-            54709 Willms Station <br /> Suite 350, Washington, USA
+          <p className="font-semibold text-lg text-gray-600">
+            {t("contact.office_title")}
           </p>
           <p className="text-gray-500">
-            Tel: (+993) 64-50-95 <br /> Email: yardam034@gmail.com
+            {t("contact.address_line1")} <br /> {t("contact.address_line2")}
+          </p>
+          <p className="text-gray-500">
+            {t("contact.phone_label")} (+993) 64-50-95 <br />{" "}
+            {t("contact.email_label")} yardam034@gmail.com
           </p>
           <p className="font-semibold text-lg text-gray-600">
-            Careers at PRESCRIPTO
+            {t("contact.careers_title")}
           </p>
-          <p className="text-gray-500">
-            Learn more about our teams and job openings.
-          </p>
+          <p className="text-gray-500">{t("contact.careers_text")}</p>
           <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
-            Explore Jobs.
+            {t("contact.explore_jobs")}
           </button>
         </div>
       </div>
