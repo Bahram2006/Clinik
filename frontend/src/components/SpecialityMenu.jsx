c13cd6a521e76954bc689fd912aeab661476595e
Bahram2006 <yardam034@gmail.com>
2026-07-08 11:36:55 +0500

    tranaltion updated

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,17 +1,19 @@
 // import React from 'react'
 import { Link } from "react-router-dom";
 import { specialityData } from "../assets/assets";
+import { useTranslation } from "react-i18next";
 
 const SpecialityMenu = () => {
+  const { t } = useTranslation();
+
   return (
     <div
       className="flex flex-col items-center gap-4 py-16 text-gray-800"
       id="speciality"
     >
-      <h1 className="text-3xl font-medium">Find by Speciality</h1>
+      <h1 className="text-3xl font-medium">{t("speciality_menu.title")}</h1>
       <p className="sm:w-1/3 text-center text-sm">
-        Simply browse through our extensive list of trusted doctors, schedule
-        your appointment hassle-free.
+        {t("speciality_menu.description")}
       </p>
       <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
         {specialityData.map((item, index) => (
@@ -22,7 +24,7 @@ const SpecialityMenu = () => {
             to={`/doctors/${item.speciality}`}
           >
             <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
-            <p>{item.speciality}</p>
+            <p>{t(`specialities.${item.speciality}`, item.speciality)}</p>
           </Link>
         ))}
       </div>
