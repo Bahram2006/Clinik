28443011a38e1e88d5aa90916990cdce1d8402ce
Bahram2006 <yardam034@gmail.com>
2026-07-08 11:28:16 +0500

    footer transalation updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 // import React from 'react'
-
 import { assets } from "../assets/assets";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="md:mx-10">
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
@@ -10,27 +12,24 @@ const Footer = () => {
         <div>
           <img className="mb-5 w-40" src={assets.logo} alt="" />
           <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
-            dignissimos, laudantium quidem odit quas a! Quasi, impedit. Nam,
-            illo. Qui, ad facilis? Ab commodi inventore numquam, perferendis
-            corrupti possimus. Maiores!
+            {t("footer.description")}
           </p>
         </div>
 
         {/* ------- Center Section ------- */}
         <div>
-          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <p className="text-xl font-medium mb-5">{t("footer.company")}</p>
           <ul className="flex flex-col gap-2 text-gray-600">
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Privacy policy</li>
+            <li>{t("footer.home")}</li>
+            <li>{t("footer.about_us")}</li>
+            <li>{t("footer.contact_us")}</li>
+            <li>{t("footer.privacy_policy")}</li>
           </ul>
         </div>
 
         {/* ------- Right Section ------- */}
         <div>
-          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <p className="text-xl font-medium mb-5">{t("footer.get_in_touch")}</p>
           <ul className="flex flex-col gap-2 text-gray-600">
             <li>+993 62 82-95-72</li>
             <li>sanlysaglyk@gmail.com</li>
@@ -41,9 +40,7 @@ const Footer = () => {
       {/* ------- Copyright Text ------- */}
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">
-          Copyright 2025@ Prescripto - All Right Reserved.
-        </p>
+        <p className="py-5 text-sm text-center">{t("footer.copyright")}</p>
       </div>
     </div>
   );
