06622d9701e2ced59d871faf51f9019d2e0e67ed
Bahram2006 <yardam034@gmail.com>
2026-07-08 11:38:10 +0500

    updated code translation

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -2,16 +2,18 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { useTranslation } from "react-i18next";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
+  const { t } = useTranslation();
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium ">Top Doctors to Book</h1>
+      <h1 className="text-3xl font-medium ">{t("top_doctors.title")}</h1>
       <p className="sm:w-1/3 text-center text-sm">
-        Simply browse through our extensive list of trusted doctors.
+        {t("top_doctors.description")}
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
@@ -26,15 +28,25 @@ const TopDoctors = () => {
             <img className="bg-blue-50 " src={item.image} alt="" />
             <div className="p-4">
               <div
-                className={`flex items-center gap-2  text-sm  text-center ${item.available ? "text-green-500" : "text-gray-500"}`}
+                className={`flex items-center gap-2  text-sm  text-center ${
+                  item.available ? "text-green-500" : "text-gray-500"
+                }`}
               >
                 <p
-                  className={`w-2 h-2 ${item.available ? "bg-green-500" : "bg-gray-500"} rounded-full`}
+                  className={`w-2 h-2 ${
+                    item.available ? "bg-green-500" : "bg-gray-500"
+                  } rounded-full`}
                 ></p>
-                <p>{item.available ? "Available" : "Not Available"}</p>
+                <p>
+                  {item.available
+                    ? t("top_doctors.available")
+                    : t("top_doctors.not_available")}
+                </p>
               </div>
               <p className="text-gray-900 text-lg font-medium">{item.name}</p>
-              <p className="text-gray-600 text-sm ">{item.speciality}</p>
+              <p className="text-gray-600 text-sm ">
+                {t(`specialities.${item.speciality}`, item.speciality)}
+              </p>
             </div>
           </div>
         ))}
@@ -46,7 +58,7 @@ const TopDoctors = () => {
         }}
         className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
       >
-        more
+        {t("top_doctors.more")}
       </button>
     </div>
   );
