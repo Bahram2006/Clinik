5a07e323ad82549800cf2418de9bf18724e1e84f
Bahram2006 <yardam034@gmail.com>
2026-08-05 14:50:11 +0500

    utils constant file updated

---
+++ mobile/src/utils/constants.js
@@ -0,0 +1,7 @@
+// src/utils/constants.js
+
+// Kompýuteriňiziň lokal IP adresi ýa-da Backend URL-iňiz:
+export const BACKEND_URL = "http://localhost:4000"; 
+
+// Razorpay açaryňyz:
+export const RAZORPAY_KEY_ID = "rzp_test_T528FaUrkV0Sq7";
